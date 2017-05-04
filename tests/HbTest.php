5e637a6eee07866fcd2545252e12298984800953
Huangbo <huangbo@yunshan.net.cn>
2017-05-04 18:35:46 +0800

    添加测试类

---
+++ tests/HbTest.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Money package.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SebastianBergmann\Money;
+
+use PHPUnit\Framework\TestCase;
+
+class HbTest extends TestCase
+{
+    /**
+     */
+    public function testFirst()
+    {
+        $a = 23;
+        $this->assertEquals(12, 12);
+
+    }
+}
